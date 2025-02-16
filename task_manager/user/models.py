3ce7002b3a6158c8d54b957f3c618f22d3cf9d61
Nic11371 <nic11371@gmail.com>
2025-02-16 22:42:02 +0300

    users_models

--- task_manager/user/models.py
+++ task_manager/user/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+
+class User(models.Model):
+    nic_name = models.CharField(max_length=20)
+    full_name = models.CharField(max_length=100)
+    created_at = models.DateTimeField(auto_now_add=True)
--- task_manager/user/views.py
+++ task_manager/user/views.py
@@ -1,8 +1,12 @@
 from django.shortcuts import render
 from django.views import View
+from task_manager.user.models import User
 
 
 class IndexUserView(View):
 
     def get(self, request, *args, **kwargs):
-        return render(request, 'users/index.html')
+        users = User.objects.all()
+        return render(request, 'users/index.html', context={
+            'users': users,
+        })
