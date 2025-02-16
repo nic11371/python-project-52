6e6dba2502a94dd068719a1a3fa8def03aac9909
Nic11371 <nic11371@gmail.com>
2025-02-16 10:35:54 +0300

    toggle language

--- task_manager/urls.py
+++ task_manager/urls.py
@@ -15,10 +15,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from task_manager.views import HomePageView
 
 urlpatterns = [
     path('', HomePageView.as_view(), name="home"),
+    path("i18n/", include("django.conf.urls.i18n")),
     path('admin/', admin.site.urls),
 ]
