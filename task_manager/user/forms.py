e81398428d5efbb5e137bf100ba464856f4431de
Nic11371 <nic11371@gmail.com>
2025-02-21 22:21:18 +0300

    fix

--- task_manager/user/forms.py
+++ task_manager/user/forms.py
@@ -6,42 +6,39 @@ from django.utils.translation import gettext as _
 
 class UserForm(UserCreationForm):
     first_name = forms.CharField(
-        label="First name",
+        label=_("First name"),
         max_length=100,
         widget=forms.TextInput(attrs={
             'class': 'form-control',
             'placeholder': _('Name')}))
     last_name = forms.CharField(
-        label="Last name",
+        label=_("Last name"),
         max_length=100,
         widget=forms.TextInput(attrs={
             'class': 'form-control',
             'placeholder': _('Last name')}))
     username = forms.CharField(
-        label="Username",
+        label=_("Username"),
         max_length=150,
         widget=forms.TextInput(attrs={
             'class': 'form-control',
             'placeholder': _('Username')}),
-        help_text="id_username",
         required=True)
     password1 = forms.CharField(
-        label="Password",
+        label=_("Password"),
         min_length=3,
         widget=forms.TextInput(attrs={
             'class': 'form-control',
             'placeholder': _('Password'),
             'autocomplete': 'new-password'}),
-        help_text="id_password1",
         required=True)
     password2 = forms.CharField(
-        label="Confirm password",
+        label=_("Confirm password"),
         min_length=3,
         widget=forms.TextInput(attrs={
             'class': 'form-control',
             'placeholder': _('Password'),
             'autocomplete': 'new-password'}),
-        help_text="id_password2",
         required=True)
 
     class Meta:
@@ -52,17 +49,16 @@ class UserForm(UserCreationForm):
 
 class LoginForm(AuthenticationForm):
     username = forms.CharField(
-        label="Username",
+        label=_("Username"),
         max_length=150,
         widget=forms.TextInput(attrs={
             'class': 'form-control',
             'placeholder': _('Username'),
             'autocapitalize': 'none',
             'autocomplete': 'username'}),
-        help_text="id_username",
         required=True)
     password = forms.CharField(
-        label="Password",
+        label=_("Password"),
         min_length=3,
         widget=forms.PasswordInput(attrs={
             'class': 'form-control',
