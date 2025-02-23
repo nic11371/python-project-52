5b41aa16ec64b35bac02177cefe0c23ffd50d36e
Nic11371 <nic11371@gmail.com>
2025-02-23 22:00:29 +0300

    test create users

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -88,14 +88,12 @@ WSGI_APPLICATION = 'task_manager.wsgi.application'
 DATABASE_URL = os.getenv('DATABASE_URL')
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default=DATABASE_URL,
+        conn_max_age=600,
+        conn_health_checks=True,
+    ),
 }
-
-if DATABASE_URL:
-    DATABASES['default'] = dj_database_url.parse(DATABASE_URL)
 
 
 # Password validation
