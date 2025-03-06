16a7395400229d7027006f11c3efebcd31f64017
Nic11371 <nic11371@gmail.com>
2025-03-06 11:21:08 +0300

    last

--- task_manager/user/forms.py
+++ task_manager/user/forms.py
@@ -1,6 +1,7 @@
 from django.contrib.auth.forms import UserCreationForm
 from django.utils.safestring import mark_safe
 from django.utils.translation import gettext_lazy as _
+
 from .models import User
 
 
@@ -11,7 +12,6 @@ class UserRegisterForm(UserCreationForm):
     def __init__(self, *args, **kwargs):
         super(UserCreationForm, self).__init__(*args, **kwargs)
         self.fields['password1'].help_text = self.formatted_help_text
-
 
     class Meta:
         model = User
