717ee4a850342471ba3b96bcc2c247cd7bc44b9b
Nic11371 <nic11371@gmail.com>
2025-03-07 20:11:16 +0300

    tests

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -94,10 +94,11 @@ WSGI_APPLICATION = 'task_manager.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
+DATABASE_URL = os.getenv('DATABASE_URL')
 
 DATABASES = {
     'default': dj_database_url.config(
-        default=os.getenv('DATABASE_URL'),
+        default=os.getenv(DATABASE_URL),
         conn_max_age=600,
         conn_health_checks=True,
     )
