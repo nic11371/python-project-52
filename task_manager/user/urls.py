529bc0634dbcf0c378b7c26151b51e73038a7adf
Nic11371 <nic11371@gmail.com>
2025-03-07 19:27:06 +0300

    test

--- task_manager/user/urls.py
+++ task_manager/user/urls.py
@@ -5,16 +5,16 @@ from .views import (
     ListUsers,
     SignUpUser,
     UpdateUser,
-    UpdateUserPassword,
+    # UpdateUserPassword,
 )
 
 urlpatterns = [
     path('', ListUsers.as_view(), name="users"),
     path('create/', SignUpUser.as_view(), name="user_create"),
     path('<int:pk>/update/', UpdateUser.as_view(), name="user_update"),
-    path(
-        '<int:pk>/update/password/', UpdateUserPassword.as_view(), 
-        name="user_update_password"),
+    # path(
+    #     '<int:pk>/update/password/', UpdateUserPassword.as_view(), 
+    #     name="user_update_password"),
     path('<int:pk>/delete/', DeleteUser.as_view(
         success_url="/users/"), name="user_delete"),
 ]
--- task_manager/user/views.py
+++ task_manager/user/views.py
@@ -1,5 +1,4 @@
 from django.contrib import messages
-from django.contrib.auth.views import PasswordChangeView
 from django.contrib.messages.views import SuccessMessageMixin
 from django.db.models import ProtectedError
 from django.shortcuts import redirect
@@ -33,22 +32,12 @@ class UpdateUser(
     extra_context = {
         'title': _('Changing user'),
         'button': _("Change"),
-        'link': _("Change password")
     }
+    form_class = UserRegisterForm
     context_object_name = 'update_user'
     template_name = 'general/general_form.html'
     success_url = reverse_lazy('users')
     success_message = _('User was changed successfully')
-    fields = ['first_name', 'last_name', 'username']
-
-
-class UpdateUserPassword(
-    AuthenticationMixin, AuthorizationMixin, SuccessMessageMixin, PasswordChangeView):
-    model = User
-    template_name = 'general/general_form.html'
-    extra_context = {'title': _('Changing password'), 'button': _("Change")}
-    success_url = reverse_lazy('logout')
-    success_message = _('Password of the user was changed successfully')
 
 
 class DeleteUser(
