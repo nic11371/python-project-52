8ca7a8d64431c1c59188e9971e4818b437fffefb
Nic11371 <nic11371@gmail.com>
2025-03-07 18:38:20 +0300

    test

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -94,14 +94,25 @@ WSGI_APPLICATION = 'task_manager.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
-DATABASE_URL = os.getenv('DATABASE_URL')
+DATABASE_TYPE = os.getenv('DATABASE_TYPE', default='postgreSQL')
+DATABASE_URL = os.getenv('DATABASE_URL', default='DATABASE_URL')
 
-DATABASES = {
+
+if DATABASE_TYPE == 'postgreSQL':
+    DATABASES = {
+        'default': dj_database_url.config(
+            default=DATABASE_URL,
+            conn_max_age=600,
+            conn_health_checks=True
+        ),
+    }
+else:
+    DATABASES = {
         'default': {
             'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': BASE_DIR / 'db.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
         }
-}
+    }
 
 
 # Password validation
