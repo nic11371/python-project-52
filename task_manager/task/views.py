a58ac3183fa67f17ef7dafefbee84126b13c5097
Nic11371 <nic11371@gmail.com>
2025-03-08 09:48:21 +0300

    tests

--- task_manager/task/views.py
+++ task_manager/task/views.py
@@ -7,9 +7,9 @@ from django.utils.translation import gettext_lazy as _
 from django.views.generic import CreateView, DeleteView, DetailView, UpdateView
 from django_filters.views import FilterView
 
-from .forms import TaskForm
 from ..views import AuthenticationMixin
 from .filters import TaskFilter
+from .forms import TaskForm
 from .models import Task
 
 
