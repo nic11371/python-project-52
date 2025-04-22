86951ad9edbd0ea6ac2dfc14561a9c68cfc7117b
Nic11371 <nic11371@gmail.com>
2025-04-22 21:42:37 +0300

    update db

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv('DJANGO_SECRET_KEY', 'default_secret_key_value')
+SECRET_KEY = os.getenv('SECRET_KEY', 'default_secret_key_value')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = bool(os.environ.get("DEBUG", default=0))
@@ -93,11 +93,11 @@ DATABASES = {
           'ENGINE': 'django.db.backends.{}'.format(
               os.getenv('DATABASE_ENGINE', 'sqlite3')
           ),
-          'NAME': os.getenv('DATABASE_NAME'),
-          'USER': os.getenv('DATABASE_USERNAME'),
-          'PASSWORD': os.getenv('DATABASE_PASSWORD'),
-          'HOST': os.getenv('DATABASE_HOST'),
-          'PORT': os.getenv('DATABASE_PORT'),
+          'NAME': os.getenv('DATABASE_NAME', 'postgres'),
+          'USER': os.getenv('DATABASE_USERNAME', 'postgres'),
+          'PASSWORD': os.getenv('DATABASE_PASSWORD', 'password'),
+          'HOST': os.getenv('DATABASE_HOST', '127.0.0.1'),
+          'PORT': os.getenv('DATABASE_PORT', '5432'),
       }
   }
 
